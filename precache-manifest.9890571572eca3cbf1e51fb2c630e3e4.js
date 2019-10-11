cdff600ea510bed4ecee6b53197faa61705f1f95
stasbar <stasbar1995@gmail.com>
2019-10-11 21:50:44 +0200

    Updates

--- precache-manifest.9890571572eca3cbf1e51fb2c630e3e4.js
+++ precache-manifest.9890571572eca3cbf1e51fb2c630e3e4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9099d1e06c751337ec7c649e647395e6",
+    "revision": "87564da0d395dfed1ea08553f0f1843d",
     "url": "/mozerpol/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mozerpol/static/css/2.06949f32.chunk.css"
   },
   {
-    "revision": "52ae7e663fab2299cbf2",
+    "revision": "fe720572ca2eead630a3",
     "url": "/mozerpol/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mozerpol/static/js/2.bacb4abe.chunk.js"
   },
   {
-    "revision": "52ae7e663fab2299cbf2",
-    "url": "/mozerpol/static/js/main.72fd4ba0.chunk.js"
+    "revision": "fe720572ca2eead630a3",
+    "url": "/mozerpol/static/js/main.636111c0.chunk.js"
   },
   {
     "revision": "b0bf23001626bccbdeb8",
